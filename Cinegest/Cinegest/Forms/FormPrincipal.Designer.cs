d9c857c6064cf109cf45fd1c44a2da5ac02f35e7
fabiocabaceira <fabiocabaceira@gmail.com>
2023-06-04 22:02:06 +0100

    Form principal - Acesso a outros formulários

--- Cinegest/Cinegest/Forms/FormPrincipal.Designer.cs
+++ Cinegest/Cinegest/Forms/FormPrincipal.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Cinemabtn = new System.Windows.Forms.Button();
+            this.Funcionariosbtn = new System.Windows.Forms.Button();
+            this.Clientesbtn = new System.Windows.Forms.Button();
+            this.Sessõesbtn = new System.Windows.Forms.Button();
+            this.Filmesbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Cinemabtn
+            // 
+            this.Cinemabtn.Location = new System.Drawing.Point(22, 12);
+            this.Cinemabtn.Name = "Cinemabtn";
+            this.Cinemabtn.Size = new System.Drawing.Size(143, 72);
+            this.Cinemabtn.TabIndex = 0;
+            this.Cinemabtn.Text = "Cinema";
+            this.Cinemabtn.UseVisualStyleBackColor = true;
+            this.Cinemabtn.Click += new System.EventHandler(this.Cinemabtn_Click);
+            // 
+            // Funcionariosbtn
+            // 
+            this.Funcionariosbtn.Location = new System.Drawing.Point(22, 324);
+            this.Funcionariosbtn.Name = "Funcionariosbtn";
+            this.Funcionariosbtn.Size = new System.Drawing.Size(143, 72);
+            this.Funcionariosbtn.TabIndex = 1;
+            this.Funcionariosbtn.Text = "Funcionários";
+            this.Funcionariosbtn.UseVisualStyleBackColor = true;
+            this.Funcionariosbtn.Click += new System.EventHandler(this.Funcionariosbtn_Click);
+            // 
+            // Clientesbtn
+            // 
+            this.Clientesbtn.Location = new System.Drawing.Point(22, 246);
+            this.Clientesbtn.Name = "Clientesbtn";
+            this.Clientesbtn.Size = new System.Drawing.Size(143, 72);
+            this.Clientesbtn.TabIndex = 2;
+            this.Clientesbtn.Text = "Clientes";
+            this.Clientesbtn.UseVisualStyleBackColor = true;
+            this.Clientesbtn.Click += new System.EventHandler(this.Clientesbtn_Click);
+            // 
+            // Sessõesbtn
+            // 
+            this.Sessõesbtn.Location = new System.Drawing.Point(22, 168);
+            this.Sessõesbtn.Name = "Sessõesbtn";
+            this.Sessõesbtn.Size = new System.Drawing.Size(143, 72);
+            this.Sessõesbtn.TabIndex = 3;
+            this.Sessõesbtn.Text = "Sessões";
+            this.Sessõesbtn.UseVisualStyleBackColor = true;
+            this.Sessõesbtn.Click += new System.EventHandler(this.Sessõesbtn_Click);
+            // 
+            // Filmesbtn
+            // 
+            this.Filmesbtn.Location = new System.Drawing.Point(22, 90);
+            this.Filmesbtn.Name = "Filmesbtn";
+            this.Filmesbtn.Size = new System.Drawing.Size(143, 72);
+            this.Filmesbtn.TabIndex = 4;
+            this.Filmesbtn.Text = "Filmes";
+            this.Filmesbtn.UseVisualStyleBackColor = true;
+            this.Filmesbtn.Click += new System.EventHandler(this.Filmesbtn_Click);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Filmesbtn);
+            this.Controls.Add(this.Sessõesbtn);
+            this.Controls.Add(this.Clientesbtn);
+            this.Controls.Add(this.Funcionariosbtn);
+            this.Controls.Add(this.Cinemabtn);
+            this.Name = "FormPrincipal";
             this.Text = "FormPrincipal";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Cinemabtn;
+        private System.Windows.Forms.Button Funcionariosbtn;
+        private System.Windows.Forms.Button Clientesbtn;
+        private System.Windows.Forms.Button Sessõesbtn;
+        private System.Windows.Forms.Button Filmesbtn;
     }
 }
--- Cinegest/Cinegest/Forms/FormPrincipal.cs
+++ Cinegest/Cinegest/Forms/FormPrincipal.cs
@@ -16,5 +16,77 @@ namespace Cinegest.Forms
         {
             InitializeComponent();
         }
+
+        //Botões
+
+        //Botão Cinema - Abre o FormCinema
+        private void Cinemabtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FormCinema FmCinemas = new FormCinema();
+                FmCinemas.ShowDialog();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocorreu um erro ao tentar abrir o formulário.");
+            }
+        }
+
+        //Botão Filmes - Abre o FormFilmes
+        private void Filmesbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FormFilmes FmFilmes = new FormFilmes();
+                FmFilmes.ShowDialog();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocorreu um erro ao tentar abrir o formulário.");
+            }
+        }
+
+        //Botão Sessões - Abre o FormSessões
+        private void Sessõesbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FormSessoes FmSessoes = new FormSessoes();
+                FmSessoes.ShowDialog();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocorreu um erro ao tentar abrir o formulário.");
+            }
+        }
+
+        //Botão Clientes - Abre o FormClientes
+        private void Clientesbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FormClientes FmClientes = new FormClientes();
+                FmClientes.ShowDialog();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocorreu um erro ao tentar abrir o formulário.");
+            }
+        }
+
+        //Botão Funcionarios - Abre o FormFuncionarios
+        private void Funcionariosbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FormFuncionarios FmFuncionarios = new FormFuncionarios();
+                FmFuncionarios.ShowDialog();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocorreu um erro ao tentar abrir o formulário.");
+            }
+        }
     }
 }
