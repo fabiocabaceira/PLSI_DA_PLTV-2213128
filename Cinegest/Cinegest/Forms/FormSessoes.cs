7c56ae152e60456f7f964b331b90aa7c2998ff1e
fabiocabaceira <fabiocabaceira@gmail.com>
2023-06-19 20:49:26 +0100

    FormSessoes - Conflitos

--- Cinegest/Cinegest/Forms/FormSessoes.cs
+++ Cinegest/Cinegest/Forms/FormSessoes.cs
@@ -38,20 +38,57 @@ namespace Cinegest.Forms
             filmescb.DisplayMember = "Nome";
         }
 
-        /// <summary>
-        /// Função que permite verificar se existe uma sessão marcada para uma determinada sala e data
-        /// </summary>
-        /// <param name="sala"></param>
-        /// <param name="dataHora"></param>
-        /// <returns></returns>
-        private int VerificarSeExisteSessaoMarcada(Sala sala, DateTime dataHora)
+        private bool VerificarSeExisteSessaoMarcada(Sala sala, DateTime dataHora, Filme filme)
         {
-            var sessoes = cinegest.Sessaos
-              .Where(s => s.SalaId == sala.Id && DbFunctions.TruncateTime(s.Datahora) == dataHora.Date && s.Datahora.Hour == dataHora.Hour && s.Datahora.Minute == dataHora.Minute)
-              .ToList();
+            if (!int.TryParse(filme.Duracao, out
+                var duracao))
+                return false; //Erro
 
-            return sessoes.Count;
+            //Calcular hora de começo/fim do filme alvo
+            //Estas sao as horas em que não pode haver outro filme
+            DateTime novoFilmeHoraComeco = dataHora;
+            DateTime novoFilmeHoraFim = dataHora;
 
+            //Os filmes desse filme
+            var sessoesNoDiaAlvo = cinegest.Sessaos.Where(s => s.SalaId == sala.Id && DbFunctions.TruncateTime(s.Datahora) == dataHora.Date).ToList();
+            if (sessoesNoDiaAlvo.Count() == 0)
+                return true;
+
+            int collisoes = 0;
+            foreach (var sessaoFilme in sessoesNoDiaAlvo)
+            {
+                if (!int.TryParse(sessaoFilme.Filme.Duracao, out
+                    var sessaoFilmeDuracao))
+                {
+                    collisoes++; //Erro
+                    continue;
+                }
+
+                DateTime sessaoFilmeHoraComeco = sessaoFilme.Datahora;
+                DateTime sessaoFilmeHoraFim = sessaoFilme.Datahora.AddMinutes(sessaoFilmeDuracao);
+
+                //casos possiveis
+                //-- sessaoFilmeHoraComeco<---novoFilmeHoraComeco--->sessaoFilmeHoraFim
+                //-- sessaoFilmeHoraComeco<---novoFilmeHoraFim--->sessaoFilmeHoraFim
+                //-- novoFilmeHoraComeco<=== [sessaoFilmeHoraComeco<------>sessaoFilmeHoraFim] ===>novoFilmeHoraFim
+                //OR:
+                //-- novoFilmeHoraComeco<=== [sessaoFilmeHoraComeco<---] ===>novoFilmeHoraFim
+                //-- novoFilmeHoraComeco<=== [--->sessaoFilmeHoraFim] ===>novoFilmeHoraFim
+                //returna falso em qualquer das opções acima
+
+                if (sessaoFilmeHoraComeco <= novoFilmeHoraComeco && novoFilmeHoraComeco <= sessaoFilmeHoraFim)
+                    collisoes++;
+                else if (sessaoFilmeHoraComeco <= novoFilmeHoraFim && novoFilmeHoraFim <= sessaoFilmeHoraFim)
+                    collisoes++;
+                else if (novoFilmeHoraComeco <= sessaoFilmeHoraComeco && sessaoFilmeHoraComeco <= novoFilmeHoraFim)
+                    collisoes++;
+                else if (novoFilmeHoraComeco <= sessaoFilmeHoraFim && sessaoFilmeHoraFim <= novoFilmeHoraFim)
+                    collisoes++;
+
+            }
+            if (collisoes != 0)
+                return false;
+            return true;
         }
 
         /// <summary>
@@ -100,13 +137,14 @@ namespace Cinegest.Forms
                         MessageBox.Show("O filme não está activo, não pode ser agendado");
                     }
                     // Verificar se a sala está ocupada a esta hora
-                    else if (VerificarSeExisteSessaoMarcada(sala, Datahora) == 0)
+                    else if (VerificarSeExisteSessaoMarcada(sala, Datahora, filme))
                     {
                         // Criar a sessão
                         int filmeId = filme.Id;
                         Sessao novaSessao = new Sessao(salaId, Datahora, preco, filmeId);
                         cinegest.Sessaos.Add(novaSessao);
                         cinegest.SaveChanges();
+                        MessageBox.Show("A sessão foi adicionada com sucesso");
                     }
                     else
                     {
